9f9765db3899e3e4e7c2598cc817bc9314fcf469
Fazenda-png <fazenda.j.p.s@hotmail.com>
2021-02-02 16:18:06 -0300

    Inicio steps op

--- features/page_objects/pages/oportunidades/oportunidades.rb
+++ features/page_objects/pages/oportunidades/oportunidades.rb
@@ -4,6 +4,9 @@ require_relative '../../sections/oportunidades/op_formulario.rb'
 module Pages
   module Oportunidades
     class Oportunidades < SitePrism::Page
+
+      set_url '/oportunidade'
+
       section :listagem, Sections::Oportunidades::ListagemOp, '.main-wrapper > .cont_geral'
       section :formulario, Sections::Oportunidades::Formulario, '#formulario_oportunidade'
     end
