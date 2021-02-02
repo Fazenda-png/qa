9c4257dad0dd1ada9be6db3a0da2b7e9aea1c63f
Fazenda-png <fazenda.j.p.s@hotmail.com>
2021-02-02 16:01:59 -0300

    Ajuste steps

--- features/step_definitions/Oportunidade/oportunidade.rb
+++ features/step_definitions/Oportunidade/oportunidade.rb
@@ -6,12 +6,12 @@ Dado("esteja na pagina de oportunidades") do
 
 end
   
-  Quando("preencher os campos {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}") do |string, string2, string3, string4, string5, string6, string7, string8, string9, string10, string11, string12, string13, string14, string15, string16|
-
+Quando("preencher os campos") do
+  pending # Write code here that turns the phrase above into concrete actions
 end
-  
-  Quando("preencher os campos de contato {string}, {string}, {string}, {string}, {string}") do |string, string2, string3, string4, string5|
 
+Quando("preencher os campos de contato") do
+  pending # Write code here that turns the phrase above into concrete actions
 end
   
   Então("deverá aparecer {string}") do |string|
