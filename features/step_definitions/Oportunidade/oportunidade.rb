f52bed6473b3774ebb9cd65a5a028b08fcd03de7
Guilherme Dalazen <guilhermebatistellad@gmail.com>
2021-02-04 23:51:38 -0300

    Correção format json para format message

--- features/step_definitions/Oportunidade/oportunidade.rb
+++ features/step_definitions/Oportunidade/oportunidade.rb
@@ -16,13 +16,13 @@ Quando("preencher os campos") do
   @form.cadastraOportunidade
 end
 
-Então("deverá aparecer {string}") do |mensagem|
-  expect(@home.message_op.text).to eql mensagem
-end
-
 Quando('preencher os campos {string}, {string}, {string}, {string}, {string}, {string}, {string}') do |nome, tipo, cpf, cnpj, email, nome_cont, email_cont|
   @form_erro = @oportunidade.formulario
   @form_erro.cadastro_error(nome, tipo, cpf, cnpj, email, nome_cont, email_cont)
+end
+
+Então("deverá aparecer {string}") do |mensagem|
+  expect(@home.message_op.text).to eql mensagem
 end
 
 Então('deverá aparecer {string} e {string}') do |mensagem, local|
